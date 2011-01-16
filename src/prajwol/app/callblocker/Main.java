6f515f13bbbdc179752082895a06ca4a60171def
prajwol <prajwol@ubuntu.ubuntu-domain>
2011-01-16 01:14:55 +0100

    - busy dialog shown while contacts are being loaded

--- src/prajwol/app/callblocker/Main.java
+++ src/prajwol/app/callblocker/Main.java
@@ -5,9 +5,16 @@
 
 package prajwol.app.callblocker;
 
+import java.util.List;
+
+import prajwol.app.callblocker.storage.MyContact;
 import prajwol.app.callblocker.storage.StorageManager;
+import android.app.Activity;
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
@@ -17,13 +24,28 @@ public class Main extends ListActivity {
 	private MyListAdapter listAdapter = null;
 	private StorageManager sm = App.getInstance().getStorageManager();
 
+	private List<MyContact> myContacts = null;
+	private Message myMessage;
+	private ProgressDialog busyDialog;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		listAdapter = new MyListAdapter(this, sm.getContacts(this));
-		setListAdapter(listAdapter);
+		busyDialog = new ProgressDialog(this);
+		busyDialog.setIndeterminate(true);
+		busyDialog.setMessage("Please wait ...");
+
+		busyDialog.show();
+		new Thread() {
+			public void run() {
+				myContacts = sm.getContacts(getMyReference());
+				myMessage = Message.obtain();
+				myMessage.setTarget(myHandler);
+				myMessage.sendToTarget();
+			}
+		}.start();
 	}
 
 	public void onBlockedClick(View v) {
@@ -33,4 +55,16 @@ public class Main extends ListActivity {
 		Log.i(TAG, phoneNumber + " is marked "
 				+ (cb.isChecked() ? "blocked" : "allowed"));
 	}
+
+	private Activity getMyReference() {
+		return this;
+	}
+
+	private Handler myHandler = new Handler() {
+		public void handleMessage(Message msg) {
+			listAdapter = new MyListAdapter(getMyReference(), myContacts);
+			setListAdapter(listAdapter);
+			busyDialog.dismiss();
+		}
+	};
 }
